05993df820f2b1b22d6ef3e46afea4c1feaba54d
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-08-11 18:52:59 -0300

    reafactor: footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
     return (
-        <footer className="border-2 border-neutral-400 py-4 px-2 mt-auto">
+        <footer className="border-2 border-neutral-400 py-4 px-2 mt-auto flex flex-col justify-center items-center gap-4">
+            <div className="flex-1 flex justify-center">
+                <Link href={"/"}>
+                    <Image
+                        src="/logo.svg"
+                        alt="Logo"
+                        width={40}
+                        height={40}
+                    />
+                </Link>
+            </div>
             <span className="font-extrabold">© 2023 Copyright Example</span>
         </footer>
     )
