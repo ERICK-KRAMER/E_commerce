85d270490837893ea6aa8df19cfcc4f0ad8db8c8
ERICK-KRAMER <kramerfilho@hotmail.com>
2024-08-15 16:36:06 -0300

    feat: add sidebar

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,20 +1,47 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./ui/sheet";
 
 const Header = () => {
     return (
         <header className="flex flex-row py-4 px-5 items-center justify-between">
-            <div className="flex-1 flex justify-start">
-                <Button variant={"link"} size={"icon"}>
-                    <Image
-                        src="/menu.svg"
-                        alt="Menu"
-                        width={30}
-                        height={30}
-                    />
-                </Button>
-            </div>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <div className="flex-1 flex justify-start">
+                        <Button variant={"link"} size={"icon"}>
+                            <Image
+                                src="/menu.svg"
+                                alt="Menu"
+                                width={30}
+                                height={30}
+                            />
+                        </Button>
+                    </div>
+                </SheetTrigger>
+                <SheetContent side={"left"} className="py-10 space-y-10 ">
+                    <SheetHeader >
+                        <span className="font-bold">
+                            <Link href={"/sign-in"} className="flex items-center gap-3">
+                                <Button variant={"link"} size={"icon"} className="bg-neutral-800 rounded-full flex justify-center items-center w-12 h-12">
+                                    <Image
+                                        src="/user.svg"
+                                        alt="User"
+                                        width={16}
+                                        height={16}
+                                    />
+                                </Button>
+                                Faça login
+                            </Link>
+                        </span>
+                    </SheetHeader>
+                    <nav className="flex flex-col gap-3">
+                        <Button variant={"link"} className="text-lg font-bold">Home</Button>
+                        <Button variant={"link"} className="text-lg font-bold">Collection</Button>
+                        <Button variant={"link"} className="text-lg font-bold">Deals</Button>
+                    </nav>
+                </SheetContent>
+            </Sheet>
             <div className="flex-1 flex justify-center">
                 <Link href={"/"}>
                     <Image
@@ -26,23 +53,13 @@ const Header = () => {
                 </Link>
             </div>
             <div className="flex-1 flex justify-end items-center gap-1">
-                <Button variant={"link"} size={"icon"} className="border-4 border-black rounded-full flex justify-center items-center w-12 h-12">
+                <Button variant={"link"} size={"icon"} className="border-4 border-neutral-800 rounded-full flex justify-center items-center w-12 h-12">
                     <Link href={"/cart"}>
                         <Image
                             src="/shoppingBag.svg"
                             alt="Shopping Bag"
                             width={40}
                             height={40}
-                        />
-                    </Link>
-                </Button>
-                <Button variant={"link"} size={"icon"} className="bg-black rounded-full flex justify-center items-center w-12 h-12">
-                    <Link href={"/sign-in"}>
-                        <Image
-                            src="/user.svg"
-                            alt="User"
-                            width={16}
-                            height={16}
                         />
                     </Link>
                 </Button>
